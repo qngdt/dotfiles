1e51b09c87617d559e91d05f7bdf4f4db5be41bd
Quang Dang <quangdt.dev@gmail.com>
2021-09-11 00:34:02 +0700

    Fix bash script

--- backup.sh
+++ backup.sh
@@ -1,12 +1,12 @@
 # Backup Neovim
-cp -R ~/.config/nvim ./nvim
+cp -R ~/.config/nvim/* ./nvim
 
 # Backup zsh
 cp ~/.p10k.zsh ./zsh
 cp ~/.zshrc ./zsh
 
 # Backup Alacritty
-cp -R ~/.config/alacritty ./alacritty
+cp -R ~/.config/alacritty/* ./alacritty
 
 # Backup Tmux
 cp ~/.tmux.conf.local ./tmux
