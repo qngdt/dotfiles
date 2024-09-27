8404e8b1baca40097bcedc76f5386710ab3ce610
qngdt <quangdt.dev@gmail.com>
2024-09-27 21:48:54 +0900

    misc: use gruvbox theme

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -852,40 +852,17 @@ require('lazy').setup({
       }
     end,
   },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    enabled = false,
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      local time = os.date '*t'
-      if time.hour >= 6 and time.hour < 18 then
-        vim.cmd.colorscheme 'tokyonight-day'
-      else
-        vim.cmd.colorscheme 'tokyonight-storm'
-      end
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
   {
-    'rebelot/kanagawa.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000,
     init = function()
       local time = os.date '*t'
       if time.hour >= 6 and time.hour < 18 then
-        vim.cmd.colorscheme 'kanagawa-lotus'
+        vim.o.background = 'light'
       else
-        vim.cmd.colorscheme 'kanagawa-wave'
+        vim.o.background = 'dark'
       end
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
   -- Highlight todo, notes, etc in comments
