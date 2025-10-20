f46f0309fa80f70ebffc64734f45fcf4ac0ff1ad
qngdt <quang.dang@primenumber.co.jp>
2025-10-20 02:53:05 +0000

    nvim: coloring and tabbing

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -589,6 +589,20 @@ require("lazy").setup({
 
 			require("mini.surround").setup()
 
+			require("mini.tabline").setup()
+
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+					hex_color = hipatterns.gen_highlighter.hex_color(),
+				},
+			})
+
 			require("mini.files").setup({
 				mappings = {
 					close = "<C-c>",
